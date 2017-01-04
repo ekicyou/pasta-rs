368cffe98ccb70f40e950d844a68f72cdfaf3c7e
ekicyou <dot.station@gmail.com>
2017-01-04 19:16:36 +0900

    更新

--- src/windows.rs
+++ src/windows.rs
@@ -1,41 +1,76 @@
 use winapi::{HGLOBAL, UINT, size_t};
-use std::ffi::OsStr;
-use gstr::*;
-use shiori3::api::*;
-use std::cell::RefCell;
 
-const GMEM_FIXED: UINT = 0;
+mod app {
+    use std::sync::RwLock;
+    use std::ffi::{OsStr, OsString};
+    use shiori3::api::*;
+    use winapi::{HGLOBAL, UINT, size_t};
+    use gstr::*;
 
-// static mut pasta: Option<RefCell<Shiori>> = None;
+    lazy_static! {
+         static ref PASTA: RwLock<Option<Shiori>>=RwLock::new(Option::None);
+    }
+
+    pub fn load(hdir: HGLOBAL, len: size_t) -> Result<(), String> {
+        let g = GStr::new(hdir, len);
+        let os_dir = g.to_os_str().unwrap();
+        let mut pasta = match PASTA.write() {
+            Ok(a) => a,
+            _ => return Err("rw_lock_poison".to_string()),
+        };
+        *pasta = Shiori::load(&os_dir);
+        match *pasta {
+            Some(_) => Ok(()),
+            _ => Err("load_error".to_string()),
+        }
+    }
+
+    pub fn unload() -> Result<(), String> {
+        Ok(())
+    }
+
+    pub fn request(h: &mut HGLOBAL, len: &mut size_t) -> Result<(), String> {
+        let g = GStr::new(*h, *len);
+        let req = match g.to_str() {
+            Ok(a) => a,
+            _ => return Err("not utf-8".to_string()),
+        };
+        Ok(())
+    }
+
+
+}
+
 
 
 #[no_mangle]
 pub extern "C" fn load(hdir: HGLOBAL, len: size_t) -> bool {
-    let g = GStr::new(hdir, len);
-    let os_dir = g.to_os_str().unwrap();
-
-    true
+    match app::load(hdir, len) {
+        Ok(_) => true,
+        _ => false,
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn unload() -> bool {
-    true
+    match app::unload() {
+        Ok(_) => true,
+        _ => false,
+    }
 }
 
 #[no_mangle]
-pub extern "C" fn request(hreq: HGLOBAL, len: &mut size_t) -> bool {
-    let g = GStr::new(hreq, *len);
-    let req = match g.to_str() {
-        Ok(s) => s,
-        _ => "",
-    };
-
-    true
+pub extern "C" fn request(h: &mut HGLOBAL, len: &mut size_t) -> bool {
+    match app::request(h, len) {
+        Ok(_) => true,
+        _ => false,
+    }
 }
 
 
 #[test]
 fn ffi_test() {
+    use gstr::*;
     {
         let dir = "dir/dir";
         let g_dir = GStr::clone_from_slice_nofree(dir.as_bytes());
