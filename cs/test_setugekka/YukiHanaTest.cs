0bba3839e8781ce06dbc8d6c37ade5181e5f8ab0
ekicyou <dot.station@gmail.com>
2017-12-04 10:52:39 +0900

    サンプルの花を作る。テスト実装。

---
+++ cs/test_setugekka/YukiHanaTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test_setugekka
+{
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using Setugekka.Tuki;
+    using Setugekka.Yuki;
+
+    [TestClass]
+    public class YukiHanaTest
+    {
+        [TestMethod]
+        public void load_yuki_hana()
+        {
+            var cts = new CancellationTokenSource();
+            try
+            {
+                var ct = cts.Token;
+
+                // create
+                var yuki = new Yuki();
+
+                // load
+                unsafe
+                {
+                    var hinst = IntPtr.Zero;
+                    var load_dir = AssemblyUtil.GetCallingAssemblyDirctory();
+                    var load_t = load_dir.ToHGlobal();
+                    var load_hg = load_t.Item1;
+                    var load_len = load_t.Item2;
+                    var rc = yuki.load(hinst.ToPointer(), load_hg.ToPointer(), load_len);
+                    rc.Is(1);
+                }
+                // unload
+                {
+                    var rc = yuki.unload();
+                    rc.Is(1);
+                }
+            }
+            finally
+            {
+                cts.Cancel();
+                cts.Dispose();
+            }
+        }
+    }
+}
