0d02e5b24c2f333320082c631c79e5909791313c
ekicyou <dot.station@gmail.com>
2018-05-31 16:53:04 +0900

    テスト実装

---
+++ pasta_di/src/shiori.rs
@@ -0,0 +1,21 @@
+use std::borrow::Cow;
+use std::ffi::OsStr;
+
+/// SHIORI Error
+#[derive(Copy, Eq, PartialEq, Clone, Debug)]
+pub enum ShioriError {
+    NotLoad,
+}
+
+/// SHIORI manage API
+pub trait ShioriAPI {
+    fn load<STR: AsRef<OsStr> + ?Sized>(&mut self, dir: &STR) -> Result<(), ShioriError>;
+    fn unload(&mut self) -> Result<(), ShioriError>;
+    fn request(&mut self, req_text: &str) -> Result<Cow<str>, ShioriError>;
+}
+
+/// have SHIORI
+pub trait HaveShioriAPI {
+    type ShioriAPI: ShioriAPI;
+    fn shiori(&self) -> Self::ShioriAPI;
+}
