c6a86e5b3ec71c60ac502dfe4f37b145f05700a4
ekicyou <dot.station@gmail.com>
2018-06-27 17:05:01 +0900

    現状でコミット

--- shiori_parser/src/lib.rs
+++ shiori_parser/src/lib.rs
@@ -1,13 +1,13 @@
-extern crate pest;
-
+#[allow(unused_imports)]
 #[macro_use]
 extern crate log;
 #[cfg(test)]
 extern crate env_logger;
+
 #[macro_use]
 extern crate pest_derive;
+extern crate pest;
 
-mod ident;
 mod shiori;
 
 pub use shiori::ShioriParser;
--- shiori_parser/src/shiori.rs
+++ shiori_parser/src/shiori.rs
@@ -1,12 +1,14 @@
-use pest::Parser;
-
 #[derive(Parser)]
 #[grammar = "shiori.pest"]
 pub struct ShioriParser;
 
-#[test]
-fn test_id() {
-    {
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pest::Parser;
+
+    #[test]
+    fn id_1() {
         let items = ShioriParser::parse(Rule::id, "a1")
             .unwrap_or_else(|e| panic!("{}", e))
             .collect::<Vec<_>>();
@@ -20,7 +22,8 @@ fn test_id() {
             assert_eq!(span.end(), 2);
         }
     }
-    {
+    #[test]
+    fn id_2() {
         let items = ShioriParser::parse(Rule::id, "感じの良いID")
             .unwrap_or_else(|e| panic!("{}", e))
             .collect::<Vec<_>>();
@@ -32,6 +35,40 @@ fn test_id() {
             assert_eq!(span.as_str(), "感じの良いID");
             assert_eq!(span.start(), 0);
             assert_eq!(span.end(), 17);
+        }
+    }
+
+    #[test]
+    fn method_1() {
+        let items = ShioriParser::parse(Rule::method, "GET")
+            .unwrap_or_else(|e| panic!("{}", e))
+            .collect::<Vec<_>>();
+        assert_eq!(items.len(), 1);
+        {
+            let pair = &items[0];
+            assert_eq!(pair.as_rule(), Rule::method);
+            let span = pair.clone().into_span();
+            assert_eq!(span.as_str(), "GET");
+            assert_eq!(span.start(), 0);
+            assert_eq!(span.end(), 3);
+            let items = pair.clone().into_inner().collect::<Vec<_>>();
+            assert_eq!(items.len(), 1);
+        }
+    }
+
+    #[test]
+    fn method_2() {
+        let items = ShioriParser::parse(Rule::method, "NOTIFY")
+            .unwrap_or_else(|e| panic!("{}", e))
+            .collect::<Vec<_>>();
+        assert_eq!(items.len(), 1);
+        {
+            let pair = &items[0];
+            assert_eq!(pair.as_rule(), Rule::method);
+            let span = pair.clone().into_span();
+            assert_eq!(span.as_str(), "NOTIFY");
+            assert_eq!(span.start(), 0);
+            assert_eq!(span.end(), 6);
         }
     }
 }
