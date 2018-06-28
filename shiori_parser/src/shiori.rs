ec390773bbccc7c3230261e5352e1b140c75c6b5
ekicyou <dot.station@gmail.com>
2018-06-28 17:24:24 +0900

    add tests

--- shiori_parser/src/shiori.rs
+++ shiori_parser/src/shiori.rs
@@ -179,5 +179,30 @@ mod tests {
         }
     }
 
+    #[test]
+    fn key_value_1() {
+        let items = ShioriParser::parse(Rule::key_value, "IDは: XYZですよ\r\n")
+            .unwrap_or_else(|e| panic!("{}", e))
+            .collect::<Vec<_>>();
+        assert_eq!(items.len(), 1);
+
+        let pair = &items[0];
+        assert_eq!(pair.as_rule(), Rule::key_value);
+        let span = pair.clone().into_span();
+        assert_eq!(span.as_str(), "IDは: XYZですよ\r\n");
+        assert_eq!(span.start(), 0);
+        assert_eq!(span.end(), 21);
+
+        let items = pair.clone().into_inner().collect::<Vec<_>>();
+        assert_eq!(items.len(), 2);
+
+        let pair = &items[0];
+        assert_eq!(pair.as_rule(), Rule::key);
+        assert_eq!(pair.as_str(), "IDは");
+
+        let pair = &items[1];
+        assert_eq!(pair.as_rule(), Rule::value);
+        assert_eq!(pair.as_str(), "XYZですよ");
+    }
 
 }
