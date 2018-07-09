215bdb6423a273cdd23cacd19c3f2d3614562bdb
ekicyou <dot.station@gmail.com>
2018-07-09 15:20:18 +0900

    テストの更新

--- shiori_parser/src/shiori.rs
+++ shiori_parser/src/shiori.rs
@@ -44,34 +44,28 @@ mod tests {
 
     #[test]
     fn remain_1() {
-        let items = ShioriParser::parse(Rule::remain, "ABC\r\n")
+        let mut items = ShioriParser::parse(Rule::remain, "ABC\r\n")
             .unwrap_or_else(|e| panic!("{}", e))
-            .collect::<Vec<_>>();
-        assert_eq!(items.len(), 1);
-        {
-            let pair = &items[0];
-            assert_eq!(pair.as_rule(), Rule::remain);
-            let span = pair.clone().into_span();
-            assert_eq!(span.as_str(), "ABC");
-            assert_eq!(span.start(), 0);
-            assert_eq!(span.end(), 3);
-        }
+            .flatten();
+
+        let pair = items.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::remain);
+        assert_eq!(pair.as_str(), "ABC");
+
+        assert_eq!(items.next(), None);
     }
 
     #[test]
     fn remain_2() {
-        let items = ShioriParser::parse(Rule::remain, "ABC\rABCD\r\n")
+        let mut items = ShioriParser::parse(Rule::remain, "ABC\rABCD\r\n")
             .unwrap_or_else(|e| panic!("{}", e))
-            .collect::<Vec<_>>();
-        assert_eq!(items.len(), 1);
-        {
-            let pair = &items[0];
-            assert_eq!(pair.as_rule(), Rule::remain);
-            let span = pair.clone().into_span();
-            assert_eq!(span.as_str(), "ABC\rABCD");
-            assert_eq!(span.start(), 0);
-            assert_eq!(span.end(), 8);
-        }
+            .flatten();
+
+        let pair = items.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::remain);
+        assert_eq!(pair.as_str(), "ABC");
+
+        assert_eq!(items.next(), None);
     }
 
     #[test]
@@ -214,41 +208,32 @@ mod tests {
 
     #[test]
     fn key_3() {
-        let items = ShioriParser::parse(Rule::key, "Reference123")
+        let mut it = ShioriParser::parse(Rule::key, "Reference123:")
             .unwrap_or_else(|e| panic!("{}", e))
-            .collect::<Vec<_>>();
-        assert_eq!(items.len(), 1);
-        {
-            let pair = &items[0];
-            assert_eq!(pair.as_rule(), Rule::key_ref);
-            let span = pair.clone().into_span();
-            assert_eq!(span.as_str(), "Reference123");
-            assert_eq!(span.start(), 0);
-            assert_eq!(span.end(), 12);
+            .flatten();
 
-            let mut it = pair.clone().into_inner();
-            let pair = it.next().unwrap();
-            assert_eq!(pair.as_rule(), Rule::nums);
-            assert_eq!(pair.as_str(), "123");
+        let pair = it.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::key_ref);
+        assert_eq!(pair.as_str(), "Reference123");
 
-            assert_eq!(it.next(), None);
-        }
+        let pair = it.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::nums);
+        assert_eq!(pair.as_str(), "123");
+
+        assert_eq!(it.next(), None);
     }
 
     #[test]
     fn key_4() {
-        let items = ShioriParser::parse(Rule::key, "Reference123の感じ")
+        let mut it = ShioriParser::parse(Rule::key, "Reference123の感じ")
             .unwrap_or_else(|e| panic!("{}", e))
-            .collect::<Vec<_>>();
-        assert_eq!(items.len(), 1);
-        {
-            let pair = &items[0];
-            assert_eq!(pair.as_rule(), Rule::key_other);
-            let span = pair.clone().into_span();
-            assert_eq!(span.as_str(), "Reference123の感じ");
-            assert_eq!(span.start(), 0);
-            assert_eq!(span.end(), 21);
-        }
+            .flatten();
+
+        let pair = it.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::key_other);
+        assert_eq!(pair.as_str(), "Reference123の感じ");
+
+        assert_eq!(it.next(), None);
     }
 
     #[test]
@@ -280,6 +265,108 @@ mod tests {
     #[test]
     fn req_1() {
         let grammar = include_str!("test_data/shiori3-1.txt");
+        let items = ShioriParser::parse(Rule::req, grammar)
+            .unwrap_or_else(|e| panic!("{}", e))
+            .collect::<Vec<_>>();
+        assert_eq!(items.len(), 1);
+
+        let pair = &items[0];
+        assert_eq!(pair.as_rule(), Rule::req);
+        let span = pair.clone().into_span();
+        assert_eq!(span.as_str(), grammar);
+        assert_eq!(span.start(), 0);
+        assert_eq!(span.end(), 82);
+
+        let items = pair.clone().into_inner().collect::<Vec<_>>();
+        assert_eq!(items.len(), 3);
+
+        let pair = &items[0];
+        assert_eq!(pair.as_rule(), Rule::header);
+        assert_eq!(pair.as_str(), "GET SHIORI/3.0\r\n");
+
+        let pair = &items[1];
+        assert_eq!(pair.as_rule(), Rule::key_values);
+        {
+            let items = pair.clone().into_inner().collect::<Vec<_>>();
+            assert_eq!(items.len(), 4);
+            let pair = &items[0];
+            assert_eq!(pair.as_rule(), Rule::key_value);
+            assert_eq!(pair.as_str(), "Charset: UTF-8\r\n");
+            let pair = &items[1];
+            assert_eq!(pair.as_rule(), Rule::key_value);
+            assert_eq!(pair.as_str(), "ID: version\r\n");
+            let pair = &items[2];
+            assert_eq!(pair.as_rule(), Rule::key_value);
+            assert_eq!(pair.as_str(), "SecurityLevel: local\r\n");
+            let pair = &items[3];
+            assert_eq!(pair.as_rule(), Rule::key_value);
+            assert_eq!(pair.as_str(), "Sender: SSP\r\n");
+        }
+
+        let pair = &items[2];
+        assert_eq!(pair.as_rule(), Rule::EOI);
+    }
+
+    #[test]
+    fn req_2() {
+        let grammar = include_str!("test_data/shiori3-2.txt");
+        let mut items = ShioriParser::parse(Rule::req, grammar)
+            .unwrap_or_else(|e| panic!("{}", e))
+            .flatten();
+
+        let pair = items.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::req);
+        let span = pair.clone().into_span();
+        assert_eq!(span.as_str(), grammar);
+        assert_eq!(span.start(), 0);
+        assert_eq!(span.end(), 133);
+
+        assert_eq!(items.next().unwrap().as_rule(), Rule::header);
+        assert_eq!(items.next().unwrap().as_rule(), Rule::method);
+        assert_eq!(items.next().unwrap().as_rule(), Rule::notify);
+        assert_eq!(items.next().unwrap().as_rule(), Rule::header3);
+        assert_eq!(items.next().unwrap().as_rule(), Rule::key_values);
+
+        assert_eq!(items.next().unwrap().as_rule(), Rule::key_value);
+        assert_eq!(items.next().unwrap().as_rule(), Rule::key_charset);
+        let pair = items.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::value);
+        assert_eq!(pair.as_str(), "UTF-8");
+
+        assert_eq!(items.next().unwrap().as_rule(), Rule::key_value);
+        assert_eq!(items.next().unwrap().as_rule(), Rule::key_sender);
+        let pair = items.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::value);
+        assert_eq!(pair.as_str(), "SSP");
+
+        assert_eq!(items.next().unwrap().as_rule(), Rule::key_value);
+        assert_eq!(items.next().unwrap().as_rule(), Rule::key_security_level);
+        let pair = items.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::value);
+        assert_eq!(pair.as_str(), "local");
+
+        assert_eq!(items.next().unwrap().as_rule(), Rule::key_value);
+        assert_eq!(items.next().unwrap().as_rule(), Rule::key_id);
+        let pair = items.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::value);
+        assert_eq!(pair.as_str(), "ownerghostname");
+
+        assert_eq!(items.next().unwrap().as_rule(), Rule::key_value);
+        assert_eq!(items.next().unwrap().as_rule(), Rule::key_ref);
+        let pair = items.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::nums);
+        assert_eq!(pair.as_str(), "0");
+        let pair = items.next().unwrap();
+        assert_eq!(pair.as_rule(), Rule::value);
+        assert_eq!(pair.as_str(), "セキュリティボール");
+
+        assert_eq!(items.next().unwrap().as_rule(), Rule::EOI);
+        assert_eq!(items.next(), None);
+    }
+
+    //#[test]
+    fn req_3() {
+        let grammar = include_str!("test_data/shiori2-1.txt");
         let items = ShioriParser::parse(Rule::req, grammar)
             .unwrap_or_else(|e| panic!("{}", e))
             .collect::<Vec<_>>();
