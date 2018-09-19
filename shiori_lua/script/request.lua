33fa0ab2f77c5b876f2ffe6007e663b759a55357
ekicyou <dot.station@gmail.com>
2018-09-19 16:59:50 +0900

    update

--- shiori_lua/script/request.lua
+++ shiori_lua/script/request.lua
@@ -2,17 +2,22 @@
 SHIORI requestを処理し、responseを返します。
 ]]
 
+local binser = require "binser"
+local res = {}
+local save = {}
+
+local function load(args)
+    res.load_dir = args.load_dir
 
 
+end
+
+local function unload()
+end
 
 local request = coroutine.wrap(
     function(req)
-        local function load()
-        end
-        local function unload()
-        end
-
-        load()
+        load(req)
         while req do
             local res = "res"
             req = coroutine.yield(res)
