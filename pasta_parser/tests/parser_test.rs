1c16ff2cc9bc58ec2a7508482136116ac4e76970
ekicyou <dot.station@gmail.com>
2020-07-14 16:18:27 +0900

    peg追加

--- pasta_parser/tests/parser_test.rs
+++ pasta_parser/tests/parser_test.rs
@@ -1,15 +1,14 @@
+use pasta_parser::{parse, Rule};
+
 #[test]
 fn char_test1() {
-    use pasta_parser::{parse, Rule};
-
     {
         let m = parse(Rule::AT, "@").unwrap();
         println!("{:?}", m);
     }
     {
-        let m = parse(Rule::AT, "＠").unwrap().next().unwrap();
+        let m = parse(Rule::AT, "＠").unwrap();
         println!("{:?}", m);
-        assert_eq!("＠", m.as_str());
     }
     {
         let m = parse(Rule::id, "id1@").unwrap().next().unwrap();
@@ -28,7 +27,21 @@ fn char_test1() {
         assert_eq!("#", m.as_str());
     }
     {
-        let m = parse(Rule::esc_char, "＠＠").unwrap().next().unwrap();
-        assert_eq!("＠＠", m.as_str());
+        // esc_char の対象文字取得
+        let m = parse(Rule::esc_char, "＠＠")
+            .unwrap()
+            .flatten()
+            .nth(1)
+            .unwrap();
+        println!("{:?}", m);
+        assert_eq!("＠", m.as_str());
+    }
+}
+
+#[test]
+fn char_test2() {
+    {
+        let m = parse(Rule::comment, "#123").unwrap().next().unwrap();
+        assert_eq!("#123", m.as_str());
     }
 }
