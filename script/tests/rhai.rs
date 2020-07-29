fb7c9b98384e76234f5d0585aaf15a6893a5b8b7
ekicyou <dot.station@gmail.com>
2020-07-29 20:58:13 +0900

    サンプル作成中

---
+++ script/tests/rhai.rs
@@ -0,0 +1,23 @@
+use rhai::{Engine, EvalAltResult, ParseErrorType, RegisterFn, INT};
+
+#[test]
+fn eval()-> Result<(), Box<EvalAltResult>> {
+    let engine = Engine::new();
+    let result = engine.eval::<INT>(
+        r"
+                fn すべての答え() { 42 }
+                すべての答え()
+            ",
+    );
+    assert_eq!(result?, 42);
+
+    let result = engine.eval::<INT>(
+        r"
+                fn _1() { 1 }
+                _1()
+            ",
+    );
+    assert!(result.is_err());
+
+    Ok(())
+}
