329380780bfc69d7615d6e0d09e731a888d13f2f
ekicyou <dot.station@gmail.com>
2020-08-28 18:05:13 +0900

    現状でコミット

--- script/src/ss_format.rs
+++ script/src/ss_format.rs
@@ -1,10 +1,65 @@
 use std::borrow::Cow;
 use std::collections::HashMap;
-use std::fmt::Display;
+use std::convert::AsRef;
+use std::fmt::{Display, Error, Formatter};
 
 #[derive(Clone, Default, Debug)]
-pub struct SakuraScriptTalkFormatter<'a> {
-    text: Cow<'a, str>,
+pub struct SakuraScriptTalk<'a> {
+    text: &'a str,
+}
+
+impl<'a> SakuraScriptTalk<'a> {
+    pub fn new(text: &'a str) -> SakuraScriptTalk<'a> {
+        SakuraScriptTalk { text: text }
+    }
+    pub fn write(f: &mut Formatter, text: &'a str) -> Result<(), Error> {
+        let a = Self::new(text);
+        write!(f, "{}", a);
+        Ok(())
+    }
+
+    fn wait(f: &mut Formatter, ms: isize) -> Result<(), Error> {
+        if ms > 0 {
+            write!(f, r#"\_w[{}]"#, ms);
+        }
+        Ok(())
+    }
+}
+
+impl<'a> Display for SakuraScriptTalk<'a> {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        let mut remain = 0;
+        for c in self.text.chars() {
+            let mut pre = 0;
+            let mut suf = 0;
+
+            match WAIT_TABLE.get(&c) {
+                None => {
+                    pre = remain;
+                    remain = 0;
+                }
+                Some(wait) if *wait == 0 => {
+                    continue;
+                }
+                Some(wait) if *wait > 0 => {
+                    if remain < *wait {
+                        remain = *wait;
+                    }
+                }
+                Some(wait) => {
+                    pre = remain;
+                    remain = 0;
+                    suf = 0 - wait;
+                }
+            }
+
+            Self::wait(f, pre);
+            write!(f, "{}", c);
+            Self::wait(f, suf);
+        }
+        Self::wait(f, remain);
+        Ok(())
+    }
 }
 
 lazy_static! {
@@ -24,7 +79,7 @@ lazy_static! {
         ii(&mut t, 400, r#"､｣"#);
         ii(&mut t, -200, r#"･"#);
 
-        ii(&mut t, -1, "\r\n");
+        ii(&mut t, 0, "\r\n");
         t
     };
 }
