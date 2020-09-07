e389296960c91d603fae65599e1241c1771e81c6
ekicyou <dot.station@gmail.com>
2020-09-07 21:35:27 +0900

    検討中。

--- script/src/ss/scene.rs
+++ script/src/ss/scene.rs
@@ -1,10 +1,15 @@
 use crate::error::*;
-use crate::ss::yield_redume;
-use futures::executor::block_on;
-use futures::task::{LocalSpawn, LocalSpawnExt};
-use rhai::{FnPtr, ImmutableString};
-
+use crate::ss::builder::SakuraScriptBuilder;
 pub use crate::ss::env::*;
+use crate::ss::yield_redume;
+use futures::channel::mpsc::{channel, Receiver, Sender};
+use futures::executor::block_on;
+use futures::sink::SinkExt;
+use futures::stream::StreamExt;
+use futures::task::{LocalSpawn, LocalSpawnExt};
+use rhai::{Dynamic, Engine, EvalAltResult, FnPtr, ImmutableString, Map, RegisterFn, AST};
+use std::cell::RefCell;
+use std::rc::Rc;
 
 /// シーン情報
 #[derive(Debug, Default, Clone)]
@@ -14,46 +19,65 @@ pub struct Scene {
 
 #[derive(Debug)]
 pub struct ScenePlayer {
-    yy: yield_redume::Yield<ImmutableString>,
     scene: Scene,
-    env: PlayEnv,
+    req: EnvDic,
+    actor: EnvDic,
+    app: EnvDic,
+    save: EnvDic,
+    eng: Rc<RefCell<Engine>>,
+    ast: Rc<RefCell<AST>>,
+    builder: SakuraScriptBuilder,
+    tx: Sender<Option<ImmutableString>>,
 }
 
 impl ScenePlayer {
     pub fn start<S: LocalSpawn>(
-        spawner: &S,
-        env: PlayEnv,
+        spawner: S,
         scene: Scene,
-    ) -> PastaResult<yield_redume::Resume<ImmutableString>> {
-        let (yy, rr) = yield_redume::yield_redume::<ImmutableString>();
+        builder: SakuraScriptBuilder,
+        eng: Rc<RefCell<Engine>>,
+        ast: Rc<RefCell<AST>>,
+    ) -> PastaResult<Receiver<Option<ImmutableString>>> {
+        let (tx, rx) = channel::<Option<ImmutableString>>(0);
+        let p = Self {
+            scene: scene,
+            req: Default::default(),
+            actor: Default::default(),
+            app: Default::default(),
+            save: Default::default(),
+            eng: eng,
+            ast: ast,
+            builder: builder,
+            tx: tx,
+        };
         let future = async move {
-            match yy.start().await {
-                Some(yy) => {
-                    let player = ScenePlayer {
-                        yy: yy,
-                        scene: scene,
-                        env: env,
-                    };
-                    player.schedule().await;
-                }
-                _ => (),
+            if let Err(e) = p.schedule().await {
+                log::error!("{}", e);
             }
         };
         spawner.spawn_local(future)?;
 
-        Ok(rr)
+        Ok(rx)
     }
 
-    async fn yy<S: Into<ImmutableString>>(&mut self, s: S) -> bool {
-        self.yy.yield_async(s.into()).await
+    async fn schedule(mut self) -> PastaResult<()> {
+        // 初回待機
+        self.tx.send(None).await?;
+        self.tx.send(None).await?;
+
+        Ok(())
     }
 
-    async fn schedule(mut self) {}
+    async fn ayield<S: Into<ImmutableString>>(&mut self, s: S) -> PastaResult<()> {
+        self.tx.send(Some(s.into())).await?;
+        self.tx.send(None).await?;
+        self.tx.send(None).await?;
+        Ok(())
+    }
 
     /// シーンをカットし、１シーン確定します。
     fn cut(&mut self) -> bool {
         let script = "スクリプト";
-
-        block_on(async move { self.yy(script).await })
+        true
     }
 }
