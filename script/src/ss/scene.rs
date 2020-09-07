ff38616ee0fd46a5a4b3ef85f860da2ab7e45817
ekicyou <dot.station@gmail.com>
2020-09-07 21:43:26 +0900

    現状でコミット

--- script/src/ss/scene.rs
+++ script/src/ss/scene.rs
@@ -2,13 +2,9 @@ use crate::error::*;
 use crate::ss::builder::SakuraScriptBuilder;
 pub use crate::ss::env::*;
 use crate::ss::yield_redume;
-use futures::channel::mpsc::{channel, Receiver, Sender};
-use futures::executor::block_on;
-use futures::sink::SinkExt;
-use futures::stream::StreamExt;
-use futures::task::{LocalSpawn, LocalSpawnExt};
 use rhai::{Dynamic, Engine, EvalAltResult, FnPtr, ImmutableString, Map, RegisterFn, AST};
 use std::cell::RefCell;
+use std::iter::Iterator;
 use std::rc::Rc;
 
 /// シーン情報
@@ -27,19 +23,23 @@ pub struct ScenePlayer {
     eng: Rc<RefCell<Engine>>,
     ast: Rc<RefCell<AST>>,
     builder: SakuraScriptBuilder,
-    tx: Sender<Option<ImmutableString>>,
+}
+
+impl Iterator for ScenePlayer {
+    type Item = ImmutableString;
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
 }
 
 impl ScenePlayer {
-    pub fn start<S: LocalSpawn>(
-        spawner: S,
+    pub fn new(
         scene: Scene,
         builder: SakuraScriptBuilder,
         eng: Rc<RefCell<Engine>>,
         ast: Rc<RefCell<AST>>,
-    ) -> PastaResult<Receiver<Option<ImmutableString>>> {
-        let (tx, rx) = channel::<Option<ImmutableString>>(0);
-        let p = Self {
+    ) -> Self {
+        Self {
             scene: scene,
             req: Default::default(),
             actor: Default::default(),
@@ -48,31 +48,7 @@ impl ScenePlayer {
             eng: eng,
             ast: ast,
             builder: builder,
-            tx: tx,
-        };
-        let future = async move {
-            if let Err(e) = p.schedule().await {
-                log::error!("{}", e);
-            }
-        };
-        spawner.spawn_local(future)?;
-
-        Ok(rx)
-    }
-
-    async fn schedule(mut self) -> PastaResult<()> {
-        // 初回待機
-        self.tx.send(None).await?;
-        self.tx.send(None).await?;
-
-        Ok(())
-    }
-
-    async fn ayield<S: Into<ImmutableString>>(&mut self, s: S) -> PastaResult<()> {
-        self.tx.send(Some(s.into())).await?;
-        self.tx.send(None).await?;
-        self.tx.send(None).await?;
-        Ok(())
+        }
     }
 
     /// シーンをカットし、１シーン確定します。
