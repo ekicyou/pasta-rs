46a40b96c1c8aedae2fe1723511007cc4ab66089
ekicyou <dot.station@gmail.com>
2020-12-01 17:26:37 +0900

    調整中

--- crates/gen/src/block.rs
+++ crates/gen/src/block.rs
@@ -1,4 +1,5 @@
 use pasta_script::ast::*;
+use std::collections::HashMap;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct RootBlock {
@@ -10,6 +11,7 @@ pub struct RootBlock {
 pub struct Attribute {
     pub name: String,
     pub id: String,
+    pub require: Vec<Require>,
     pub either: Vec<Either>,
     pub action: Vec<Action>,
     pub memory: Vec<Memory>,
@@ -22,6 +24,7 @@ impl Attribute {
         Self {
             name: name,
             id: id,
+            require: Vec::new(),
             either: Vec::new(),
             action: Vec::new(),
             memory: Vec::new(),
@@ -29,6 +32,67 @@ impl Attribute {
             error: None,
             comment: None,
         }
+    }
+}
+
+pub trait AttributeBlock {
+    fn attr(&self) -> &Attribute;
+    fn attr_mut(&mut self) -> &mut Attribute;
+    fn id_prefix(&mut self, text: &str) {
+        let mut attr = self.attr_mut();
+        attr.id = format!("{}_{}", text, &attr.id);
+    }
+    fn id_suffix(&mut self, text: &str) {
+        let mut attr = self.attr_mut();
+        attr.id = format!("{}_{}", &attr.id, text);
+    }
+    fn id_num(&mut self, prefix: &str, i: u32) {
+        self.id_prefix(prefix);
+        let suffix = match i {
+            0 => {
+                return;
+            }
+            _ => format!("{}", i),
+        };
+        self.id_suffix(&suffix);
+    }
+    fn fix_id<A: AttributeBlock>(prefix: &str, items: Vec<A>) {
+        let mut map: HashMap<String, Vec<&Attribute>> = HashMap::new();
+        for item in &items {
+            let k = item.attr().id.to_owned();
+            let v = map.entry(k).or_insert(Vec::new());
+            v.push(item.attr());
+        }
+        for (k, v) in &map {
+            if v.len() == 0 {
+                continue;
+            }
+            for _item in *v {}
+        }
+    }
+}
+impl AttributeBlock for HasiraBlock {
+    fn attr(&self) -> &Attribute {
+        &self.attr
+    }
+    fn attr_mut(&mut self) -> &mut Attribute {
+        &mut self.attr
+    }
+}
+impl AttributeBlock for AnchorBlock {
+    fn attr(&self) -> &Attribute {
+        &self.attr
+    }
+    fn attr_mut(&mut self) -> &mut Attribute {
+        &mut self.attr
+    }
+}
+impl AttributeBlock for ActorBlock {
+    fn attr(&self) -> &Attribute {
+        &self.attr
+    }
+    fn attr_mut(&mut self) -> &mut Attribute {
+        &mut self.attr
     }
 }
 
@@ -77,6 +141,7 @@ struct Builder {
     l3: Option<AnchorBlock>,
     l4: Option<ActorBlock>,
     l5: Option<SerifBlock>,
+    hv: [Option<Hasira>; 4],
 }
 
 impl Builder {
@@ -91,6 +156,7 @@ impl Builder {
             l3: None,
             l4: None,
             l5: None,
+            hv: [None, None, None, None],
         }
     }
     fn root(self) -> RootBlock {
@@ -98,16 +164,17 @@ impl Builder {
     }
 
     /// l2 -> commit
-    fn commit2(&mut self) {
-        self.commit3();
+    fn commit2(mut self) -> Self {
+        self = self.commit3();
         let item = self.l2.take();
         if let Some(item) = item {
             self.l1.hasira.push(item)
         }
+        self
     }
     /// l3 -> commit
-    fn commit3(&mut self) {
-        self.commit4();
+    fn commit3(mut self) -> Self {
+        self = self.commit4();
         let item = self.l3.take();
         let own = &mut self.l2;
         if let Some(item) = item {
@@ -115,10 +182,11 @@ impl Builder {
                 own.items.push(item);
             }
         }
+        self
     }
     /// l4 -> commit
-    fn commit4(&mut self) {
-        self.commit5();
+    fn commit4(mut self) -> Self {
+        self = self.commit5();
         let item = self.l4.take();
         let own = &mut self.l3;
         if let Some(item) = item {
@@ -126,9 +194,10 @@ impl Builder {
                 own.items.push(item);
             }
         }
+        self
     }
     /// l5 -> commit
-    fn commit5(&mut self) {
+    fn commit5(mut self) -> Self {
         let item = self.l5.take();
         let own = &mut self.l4;
         if let Some(item) = item {
@@ -136,11 +205,103 @@ impl Builder {
                 own.items.push(item);
             }
         }
+        self
+    }
+
+    fn push_error(mut self, ast: &AST) -> Self {
+        match ast.clone() {
+            AST::Error(a) => {
+                if let Some(target) = self.l5.as_mut() {
+                    target.items.push(SerifItem::Error(a));
+                } else if let Some(target) = self.l4.as_mut() {
+                    target.attr.error = Some(a);
+                } else if let Some(target) = self.l3.as_mut() {
+                    target.attr.error = Some(a);
+                } else if let Some(target) = self.l2.as_mut() {
+                    target.attr.error = Some(a);
+                }
+            }
+            _ => panic!(),
+        };
+        self
+    }
+
+    fn push_comment(mut self, ast: &AST) -> Self {
+        match ast.clone() {
+            AST::Comment(a) => {
+                if let Some(target) = self.l5.as_mut() {
+                    target.items.push(SerifItem::Comment(a));
+                } else if let Some(target) = self.l4.as_mut() {
+                    target.attr.comment = Some(a);
+                } else if let Some(target) = self.l3.as_mut() {
+                    target.attr.comment = Some(a);
+                } else if let Some(target) = self.l2.as_mut() {
+                    target.attr.comment = Some(a);
+                }
+            }
+            _ => panic!(),
+        };
+        self
+    }
+
+    /// l1 push
+    fn push_doc_comment(mut self, ast: &AST) -> Self {
+        match ast.clone() {
+            AST::DocComment(a) => self.l1.doc_comment = Some(a),
+            _ => panic!(),
+        };
+        self
+    }
+
+    /// l2 push
+    fn push_hasira(mut self, ast: &AST) -> Self {
+        self = self.commit2();
+        match ast.clone() {
+            AST::Hasira(h) if h.level >= 4 => {
+                self.hv[0] = Some(h);
+                self.hv[1] = None;
+                self.hv[2] = None;
+            }
+            AST::Hasira(h) if h.level == 3 => {
+                self.hv[1] = Some(h);
+                self.hv[2] = None;
+            }
+            AST::Hasira(h) if h.level == 2 => {
+                self.hv[2] = Some(h);
+            }
+            AST::Hasira(h) if h.level == 1 => {
+                let name = h.name.to_owned();
+                let id = format!("H{}", &name);
+                self.hv[3] = Some(h);
+                let mut l2 = HasiraBlock {
+                    attr: Attribute::new(name, id),
+                    items: Vec::new(),
+                };
+                for hv in &self.hv {
+                    if let Some(hv) = hv.as_ref() {
+                        for ast in &hv.attrs {
+                            let ast = (**ast).clone();
+                            match ast {
+                                AST::Require(a) => l2.attr.require.push(a),
+                                AST::Either(a) => l2.attr.either.push(a),
+                                AST::Action(a) => l2.attr.action.push(a),
+                                AST::Memory(a) => l2.attr.memory.push(a),
+                                AST::Forget(a) => l2.attr.forget.push(a),
+                                _ => panic!(),
+                            }
+                        }
+                    }
+                }
+                self.l2 = Some(l2);
+            }
+            _ => panic!(),
+        };
+        self
     }
 
     /// l3 push
-    fn push_anchor(&mut self, ast: &AST) {
-        self.commit3();
+    fn push_anchor(mut self, ast: &AST) -> Self {
+        self = self.commit3();
         let l3 = match ast.clone() {
             AST::Anchor(a) => {
                 let name = match &*a.expr {
@@ -156,11 +317,12 @@ impl Builder {
             _ => panic!(),
         };
         self.l3 = Some(l3);
+        self
     }
 
     /// l4 push
-    fn push_actor(&mut self, ast: &AST) {
-        self.commit4();
+    fn push_actor(mut self, ast: &AST) -> Self {
+        self = self.commit4();
         let l4 = match ast.clone() {
             AST::Hasira(h) if h.level == 0 => ActorBlock {
                 attr: Attribute::new(h.name.to_owned(), format!("A{}", h.name)),
@@ -169,10 +331,11 @@ impl Builder {
             _ => panic!(),
         };
         self.l4 = Some(l4);
+        self
     }
     /// l5 push
-    fn push_jump(&mut self, ast: &AST) {
-        self.commit5();
+    fn push_jump(mut self, ast: &AST) -> Self {
+        self = self.commit5();
         let mut l5 = SerifBlock { items: Vec::new() };
         let item = match ast.clone() {
             AST::LongJump(a) => SerifItem::LongJump(a),
@@ -181,10 +344,11 @@ impl Builder {
         };
         l5.items.push(item);
         self.l5 = Some(l5);
+        self
     }
     /// l5 push
-    fn push_hasira(&mut self, togaki: &Togaki) {
-        self.commit5();
+    fn push_serif(mut self, togaki: &Togaki) -> Self {
+        self = self.commit5();
         let mut l5 = SerifBlock { items: Vec::new() };
         for ast in &togaki.items {
             let item = match ast.clone() {
@@ -193,8 +357,6 @@ impl Builder {
                 AST::Either(a) => SerifItem::Either(a),
                 AST::Forget(a) => SerifItem::Forget(a),
                 AST::Memory(a) => SerifItem::Memory(a),
-                AST::LongJump(a) => SerifItem::LongJump(a),
-                AST::ShortJump(a) => SerifItem::ShortJump(a),
                 AST::Serif(a) => SerifItem::Serif(a),
                 AST::Comment(a) => SerifItem::Comment(a),
                 AST::Error(a) => SerifItem::Error(a),
@@ -203,14 +365,31 @@ impl Builder {
             l5.items.push(item)
         }
         self.l5 = Some(l5);
+        self
     }
 }
 
+/// script to root block
 pub fn scan(script: &Script) -> RootBlock {
-    let mut l1 = RootBlock {
-        doc_comment: None,
-        hasira: Vec::new(),
-    };
-
-    l1
+    let mut builder = Builder::new();
+    for ast in script {
+        builder = match ast {
+            AST::DocComment(_) => builder.push_doc_comment(ast),
+            AST::Comment(_) => builder.push_comment(ast),
+            AST::Error(_) => builder.push_error(ast),
+            AST::Hasira(h) => {
+                if h.level == 0 {
+                    builder.push_actor(ast)
+                } else {
+                    builder.push_hasira(ast)
+                }
+            }
+            AST::Anchor(_) => builder.push_anchor(ast),
+            AST::ShortJump(_) => builder.push_jump(ast),
+            AST::LongJump(_) => builder.push_jump(ast),
+            AST::Togaki(t) => builder.push_serif(t),
+            _ => builder,
+        }
+    }
+    builder.root()
 }
--- crates/gen/src/gen.rs
+++ crates/gen/src/gen.rs
@@ -1,51 +1,35 @@
+use crate::block;
 use crate::format_ident::*;
 use pasta_script::ast::*;
 use squote::{quote, Ident, Literal, TokenStream};
+
 use std::iter::{FromIterator, Iterator};
+use syn;
 
 pub fn gen_script(script: &Script) -> TokenStream {
     let mut ts = TokenStream::new();
 
-    for ast in script {
-        match ast {
-            AST::DocComment(a) => doc_comment(&mut ts, a),
-            AST::Comment(a) => comment(&mut ts, a),
-            AST::Error(a) => error(&mut ts, a),
-            AST::Hasira(h) => {}
-            AST::Anchor(a) => {}
-            AST::ShortJump(j) => {}
-            AST::LongJump(j) => {}
-            AST::Togaki(t) => {}
-            _ => (),
-        }
+    let root = block::scan(script);
+
+    if let Some(doc_comment) = root.doc_comment {
+        let text = doc_comment.comment.trim();
+        ts.combine(&quote! {
+            #[doc=#text]
+        });
     }
 
     ts
 }
 
-fn doc_comment(ts: &mut TokenStream, item: &DocComment) {
-    for line in item.comment.lines() {
-        ts.append(Ident::new(format!("//! {}\n", line)));
-    }
-}
-
-fn comment(ts: &mut TokenStream, item: &Comment) {
-    ts.append(Ident::new(format!("// {}\n", item.comment)));
-}
-
-fn error(ts: &mut TokenStream, item: &Error) {
-    ts.append(Ident::new(format!(
-        "// [ERROR!]({}-{}), '{}' :{}]\n",
-        item.start, item.end, item.error_token, item.error_str
-    )));
-}
-
 #[test]
-fn doc_comment_test() {
-    let mut ts = TokenStream::new();
-    let doc = DocComment {
-        comment: "コメントです\n２行目です\n３行目です".to_owned(),
-    };
-    doc_comment(&mut ts, &doc);
-    println!("{}", ts);
+fn gen_script_test() {
+    use pasta_script::*;
+    let text = r##"
+どきゅめんと
+"##;
+    let node = parse_one(Rule::script, text).unwrap();
+    if let AST::Script(ref script) = PastaParser::script(node).unwrap() {
+        let ts = gen_script(script);
+        println!("ts={}", ts);
+    }
 }
