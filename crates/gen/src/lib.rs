61ef117191aeccdf2cc57150a80850a0fd11bc75
ekicyou <dot.station@gmail.com>
2020-12-18 17:21:23 +0900

    ビルド成功

--- crates/gen/src/lib.rs
+++ crates/gen/src/lib.rs
@@ -3,6 +3,26 @@ mod h_checks;
 mod rand_jump;
 mod script;
 mod utils;
+mod word;
 
 pub use crate::script::gen_code as gen_pasta_code;
 pub use crate::script::gen_codes as gen_pasta_codes;
+pub use crate::word::gen_word_dic;
+pub use crate::word::new_word_dic;
+pub use crate::word::push_word_dic;
+
+use squote::{quote, TokenStream};
+pub fn gen(pasta_codes: &[String], csv_codes: &[String]) -> TokenStream {
+    let pasta = gen_pasta_codes(pasta_codes);
+
+    let mut dic = new_word_dic();
+    for csv in csv_codes {
+        push_word_dic(&mut dic, csv);
+    }
+    let word = gen_word_dic(dic);
+
+    quote! {
+        #pasta
+        #word
+    }
+}
---
+++ crates/gen/src/word.rs
@@ -0,0 +1,42 @@
+use csv;
+use pasta_core::WordDic;
+use squote::{quote, TokenStream};
+
+pub fn new_word_dic() -> WordDic {
+    WordDic::new()
+}
+
+pub fn push_word_dic(dic: &mut WordDic, data: &str) {
+    let mut rdr = csv::ReaderBuilder::new()
+        .comment(Some(b'#'))
+        .has_headers(true)
+        .trim(csv::Trim::All)
+        .terminator(csv::Terminator::CRLF)
+        .from_reader(data.as_bytes());
+    let header = rdr.headers().unwrap();
+    let header: Vec<_> = header.into_iter().map(|a| a.to_owned()).collect();
+    for rec in rdr.records() {
+        let rec = rec.unwrap();
+        let rec: Vec<_> = rec.into_iter().collect();
+        dic.push(header.as_slice(), rec.as_slice());
+    }
+}
+
+pub fn gen_word_dic(dic: WordDic) -> TokenStream {
+    let mut push = TokenStream::new();
+    let map = dic.map();
+    for (k, items) in map {
+        for v in items {
+            push.combine(&quote! {
+                dic.push(#k, #v);
+            });
+        }
+    }
+    quote! {
+        pub fn create_word_dic() -> ::pasta_core::WordDic{
+            let mut dic = ::pasta_core::WordDic::new();
+            #push
+            dic
+        }
+    }
+}
